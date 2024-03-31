19bb0c05ea5a2436a3323222a78aac145fd2aae5
cindyjanevick <cindyjanevick@gmail.com>
2024-03-30 19:59:32 -1000

    final project draft

---
+++ final project/script.js
@@ -0,0 +1,54 @@
+// Event listener for season filter dropdown
+const seasonDropdown = document.getElementById("season");
+seasonDropdown.addEventListener("change", () => {
+  const selectedSeason = seasonDropdown.value;
+  display_des(jsonLoc, selectedSeason); // Show all designs if 'All' is selected
+});
+
+// Display all designs initially on page load
+
+const jsonLoc = "patterns.json";
+
+const displayDesigns = (designs, season) => {
+  const designsContainer = document.getElementById("designs");
+  designsContainer.innerHTML = ""; // Clear existing designs
+  designs.forEach((design) => {
+    if (season == "all" || design.seasons.includes(season)) {
+      const designCard =
+        `
+          <div class="design-card">
+            <img src="${design.image}" alt="${design.name}">
+            <h2>${design.name}</h2>
+            <p><strong>Season:</strong> ` +
+        design.seasons.join(", ") +
+        `</p> 
+            <p><strong>Materials:</strong> </p> ` +
+        "<li> Yarn: " +
+        design.materials.yarn.join(", ") +
+        ".</li>" +
+        `
+            <p><strong>Instructions:</strong> </p> ` +
+        "<li>" +
+        design.step.join(".</li> \n <li>") +
+        ".</li>" +
+        `
+          </div>
+        `;
+      designsContainer.innerHTML += designCard;
+    }
+  });
+};
+
+function display_des(url, season) {
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => displayDesigns(data, season))
+    .catch((error) => console.error("Unable to fetch data:", error));
+}
+
+display_des(jsonLoc, "all");
